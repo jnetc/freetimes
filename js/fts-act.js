db264f08951f5b5bbcd5b4eb047b019c3c8695a3
ЖнецЪ <apxdemon@gmail.com>
2018-04-04 11:36:27 +0300

    переделка анимации мероприятий + начало курсов

--- js/fts-act.js
+++ js/fts-act.js
@@ -1,37 +1,94 @@
-let patyAdd = document.querySelectorAll('.paty-add');
-let patyBtn = document.querySelectorAll('.paty-bt');
-let numAdd = 1;
-let numBtn = 1;
+window.onload = () => {
 
-// Set attributes in animated block
-for (let attr of patyAdd) {
-  let attrAdd = attr.setAttribute("id", "tag"+numAdd);
-  numAdd++;
-}
-// Set attributes buttons
-for (let attr of patyBtn) {
-  let attrBtn = attr.setAttribute("data-id", "tag"+numBtn);  
-  numBtn++;
-}
-  
-// Loop for buttons
-for (const btn of patyBtn) {    
-  btn.onclick = () => {
-    btn.classList.toggle('pressed');
-    let btnId = btn.getAttribute('data-id');
-    
-    // loop for blocks
-    let arrayAdd = []; // create empty array
-    
-    for (let i = 0; i < patyAdd.length; i++) {    
-      let addId = patyAdd[i].getAttribute('id'); // getting attributes
-      arrayAdd.push(addId); // puting in array
+  let patyAdd   = document.querySelectorAll('.paty-add'),
+      plusBts   = document.querySelectorAll('.plus-bt'),
+      moreBts    = document.querySelectorAll('.more-bt'),
+      contents  = document.querySelectorAll('.paty-text'),
+      eventBox  = document.querySelectorAll('.next-box'),
+      numAdd    = 1,
+      numBtn    = 1;
 
-      // Check for validate
-      if (btnId == arrayAdd[i]) {
-        console.log('done');
-        patyAdd[i].classList.toggle('paty-soc-anim');  
-      }
-    }    
+    // Set attributes in animated block
+  for (let attr of patyAdd) {
+    let attrAdd = attr.setAttribute("id", "tag"+numAdd);
+    numAdd++;
   }
+    // Set attributes buttons
+  for (let attr of plusBts) {
+    let attrBtn = attr.setAttribute("data-id", "tag"+numBtn);  
+    numBtn++;
+  }
+    
+    // Loop for button "plus contact"
+  for (const btn of plusBts) {    
+    btn.onclick = () => {
+      btn.classList.toggle('pressed-plus');
+      let btnId = btn.getAttribute('data-id');
+      
+      // loop for blocks
+      let arrayAdd = []; // create empty array
+      
+      for (let i = 0; i < patyAdd.length; i++) {    
+        let addId = patyAdd[i].getAttribute('id'); // getting attributes
+        arrayAdd.push(addId); // puting in array
+
+        // Check for validate
+        if (btnId == arrayAdd[i]) {
+          // console.log('done');
+          patyAdd[i].classList.toggle('action');  
+        }
+      }    
+    }
+  }
+
+    // Loop for button "more info"
+  for (let i = 0; i < contents.length; i++) {
+     
+    let contHeight  = contents[i].offsetHeight;  // Content height
+    let contChild   = contents[i].childNodes; // Content paragraph child
+
+      // Divide height paragraphs
+    paragArr = [];
+    for (let j = 0; j < contChild.length; j++) {
+      let paragHeigths = contChild[j].offsetHeight;
+      paragArr.push(paragHeigths);
+    }
+      // Math height paragraps
+    let paragSum = paragArr.reduce( (a, b) => {return a + b;})
+
+    for (let k = 0; k < moreBts.length; k++) {
+
+        // ToDo if paragraphs more than 435px or not
+      if (contHeight < paragSum && i == k) {
+        moreBts[k].onclick = () => {
+
+          moreBts[k].classList.toggle('pressed-more');
+          let contAtr = getComputedStyle(contents[i]).height; // Check height from style
+           
+          if ((contHeight+'px') == contAtr) {
+            let log = contents[i].style.height = paragSum+"px";
+            
+          } else if ((contHeight+'px') < contAtr) {
+            let log2 = contents[i].style.height = contHeight+"px";
+          }
+        }
+      } else if (paragSum <= contHeight && i == k){
+        moreBts[k].style.display = "none";
+      }
+    }
+  }
+
+    // Show events info
+  for (let q = 0; q < eventBox.length; q++) {
+    eventBox[q].onclick = () => {
+      eventBox[q].classList.toggle('show-info');
+    }
+    eventBox[q].onmouseleave = () => {
+      eventBox[q].classList.remove('show-info');
+    }
+  }  
 }
+
+
+
+
--- js/fts-dom.js
+++ js/fts-dom.js
@@ -1,59 +1,72 @@
-window.onload = function () {
+let patyImgEl = document.querySelectorAll('.paty-box'), // Add poster image
+    patyDateEls = document.querySelectorAll('.paty-dat'), // Add data, time and caption
+    patyContEl = document.querySelectorAll('.paty-text'), // Add event text paragraph
+    patyPriceEl = document.querySelectorAll('.paty-tag'), // Add event price
+    patyLinkMap = document.querySelectorAll('.link-map'), // Create event links
+    patyLinkTel = document.querySelectorAll('.link-tel'), // ---
+    patyLinkSoc = document.querySelectorAll('.link-soc'), // ---
+    nextBlocks = document.querySelectorAll('.next-blk'); // Dynamically generate next event boxes 
 
-    
-  let patyImgEl = document.querySelectorAll('.paty-box'); // Add poster image
-  let patyDateEls = document.querySelectorAll('.paty-dat'); // Add data, time and caption
-  let patyContEl = document.querySelectorAll('.paty-text'); // Add event text paragraph
-  let patyPriceEl = document.querySelectorAll('.paty-tag'); // Add event price
-  let patyLinkMap = document.querySelectorAll('.link-map'); // Create patyent links
-  let patyLinkTel = document.querySelectorAll('.link-tel'); // ---
-  let patyLinkSoc = document.querySelectorAll('.link-soc'); // ---
-  // console.log(patyLinkMap);
-  
-    // Create new object
-  let dataJson = new XMLHttpRequest();
-    //  open( type, url/file, asunc);
-  dataJson.open("GET", "../json/cart.json", true);
-  dataJson.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      let ourData = JSON.parse(dataJson.responseText);
-      renderHtml(ourData);
-    }
-  };
-  // send request
-  dataJson.send();
-  
-    //Цикл текущих мероприятий
-  let renderHtml = data => {
-   
-    keyArr = []; // Create empty array
-    for (let key in data) {
-      // console.log(data[key].patyCont[0]);
-      keyArr.push(key); // Putting value in array
+  // Create new object
+let dataJson = new XMLHttpRequest();
+  //  open( type, url/file, asunc);
+dataJson.open("GET", "../json/cart.json", true);
+dataJson.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    let ourData = JSON.parse(dataJson.responseText);
+    renderHtml(ourData);
+  }
+};
+// send request
+dataJson.send();
 
-      let iArrKey = []; // Create empty array
-      for (let i = 0; i < patyImgEl.length; i++) { // Putting any selector in loop, because it's same length
-        iArrKey.push(i); // Putting value in array
+let renderHtml = data => {
+  
+  keyArr = []; // Create empty array
+  for (let key in data) {
+    // console.log(data[key].patyEvents[0]);
+    keyArr.push(key); // Putting value in array
+
+    let iArrKey = []; // Create empty array
+    for (let i = 0; i < patyImgEl.length; i++) { // Putting any selector in loop, because it's same length
+      iArrKey.push(i); // Putting value in array
+      
+      if (keyArr[key] == iArrKey[i]) {
+        // insertAdjacentHTML ('position',  'text')
+        patyImgEl[i].insertAdjacentHTML('afterbegin', `<img src="${data[key].patyImg}" alt="patyent">`)
+        patyDateEls[i].insertAdjacentHTML('afterbegin', `<span>${data[key].patyDate}</span>`)
+        patyDateEls[i].insertAdjacentHTML('beforeend', `<time>${data[key].patyTime}</time>`)
+        patyDateEls[i].insertAdjacentHTML('afterend', `<h3>${data[key].patyCap}</h3>`)
         
-        if (keyArr[key] == iArrKey[i]) {
-          // insertAdjacentHTML ('position',  'text')
-          patyImgEl[i].insertAdjacentHTML('afterbegin', `<img src="${data[key].patyImg}" alt="patyent">`)
-          patyDateEls[i].insertAdjacentHTML('afterbegin', `<span>${data[key].patyDate}</span>`)
-          patyDateEls[i].insertAdjacentHTML('beforeend', `<time>${data[key].patyTime}</time>`)
-          patyDateEls[i].insertAdjacentHTML('afterend', `<h3>${data[key].patyCap}</h3>`)
-          
-            // Loop for paragraphs array
-          let contentText = data[key].patyCont[0];
-            for (let paragraph in contentText) {
-              patyContEl[i].innerHTML += `<li>${contentText[paragraph]}</li>`
-            }
+          // Loop for paragraphs array
+        let contentText = data[key].patyCont[0];
+          for (let paragraph in contentText) {
+            patyContEl[i].setAttribute("style", "height: 345px");
+            patyContEl[i].innerHTML += `<li class="liTxt">${contentText[paragraph]}</li>`
+          }
 
-          patyPriceEl[i].insertAdjacentHTML('beforeend', `<span>${data[key].patyPrice}<sup>€</sup></span>`)
-          patyLinkMap[i].href = `${data[key].patyMap}` // Change just link in tag 'a' 
-          patyLinkTel[i].href = `tel:${data[key].patyTel}`
-          patyLinkSoc[i].href = `${data[key].patySoc}`
-        } 
-      }
+        patyPriceEl[i].insertAdjacentHTML('beforeend', `<span>${data[key].patyPrice}<sup>€</sup></span>`)
+        patyLinkMap[i].href = `${data[key].patyMap}` // Change just link in tag 'a' 
+        patyLinkTel[i].href = `tel:${data[key].patyTel}`
+        patyLinkSoc[i].href = `${data[key].patySoc}`
+        
+          // EVENT BOXES
+        let eventsEl = data[key].patyEvents[0];
+        for (let eventNum in eventsEl) {
+          nextBlocks[i].innerHTML += `
+          <div class="next-box">
+            <span class="next-date">
+              <span>${eventsEl[eventNum][0].eventDate}</span>${eventsEl[eventNum][0].eventMonth}
+            </span>
+            <figure>
+              <img src="${eventsEl[eventNum][0].eventPoster}" alt="next event">
+            </figure>
+            <h5>${eventsEl[eventNum][0].eventCap}</h5>
+            <span class="next-btn">${eventsEl[eventNum][0].eventBtn}</span>
+            <p>${eventsEl[eventNum][0].eventCont}</p> 
+          </div>`
+        }
+      } 
     }
   }
 }
