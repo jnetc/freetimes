ff053fcd65ab0ed587d24a6d7e7c7b3241585d2b
ЖнецЪ <apxdemon@gmail.com>
2018-04-05 16:10:03 +0300

    счетчик

--- js/fts-dom.js
+++ js/fts-dom.js
@@ -5,7 +5,8 @@ let patyImgEl = document.querySelectorAll('.paty-box'), // Add poster image
     patyLinkMap = document.querySelectorAll('.link-map'), // Create event links
     patyLinkTel = document.querySelectorAll('.link-tel'), // ---
     patyLinkSoc = document.querySelectorAll('.link-soc'), // ---
-    nextBlocks = document.querySelectorAll('.next-blk'); // Dynamically generate next event boxes 
+    nextBlocks = document.querySelectorAll('.next-blk'), // Dynamically generate next event boxes
+    coursesDom = document.querySelector('.courses'); // Dynamically generate courses
 
   // Create new object for events
 let eventsData = new XMLHttpRequest();
@@ -75,7 +76,6 @@ let renderEvents = data => {
 
   // Create new object for courses
 let coursesData = new XMLHttpRequest();
-  //  open( type, url/file, asunc);
 coursesData.open("GET", "../json/courses.json", true);
 coursesData.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
@@ -84,84 +84,80 @@ coursesData.onreadystatechange = function () {
     // console.log(crsParse);
     // let btnJsonParse = document.querySelector('#btJsonParse');
     // btnJsonParse.addEventListener('click', function () {
-    //   let crsBnt = renderCourses(crsParse);
+    //   let crsBnt = renderCourses(crsParse);   
     // });  
   }
 };
-// send request
 coursesData.send();
 
-let coursesDom = document.querySelector('.courses');
+
 let renderCourses = crs => {
-
-  let keyArr = [];
+  keyArr = [];
   for (let key in crs) {
-  keyArr.push(key);
-    
-    coursesDom.innerHTML += 
-    `<div class="flex-crs">
-      <div class="crs-dat">
-        <span class="crs-cls">${crs[key].Cls}</span> 
-        <img src="${crs[key].Img}" alt="courses">
-      </div>
-      <div class="crs-cont">
-        <h4 class="crs-name">${crs[key].Name}</h4>
-        <ul class="crs-graphs"></ul>
-      </div>
-      <div class="crs-opt">
-        <div class="crs-btns"></div>
-        <div class="for-price">
-          <span class="crs-tip">${crs[key].Period}</span>
-          <span class="crs-price">${crs[key].Price}<sup>€</sup></span>
+    // console.log(key);
+    keyArr.push(key);
+        // Create main DOM of courses
+      coursesDom.innerHTML += 
+      `<div class="flex-crs">
+        <div class="crs-dat">
+          <span class="crs-cls">${crs[key].Cls}</span> 
+          <img src="${crs[key].Img}" alt="courses">
         </div>
-      </div>
-    </div>`;
-
+        <div class="crs-cont">
+          <h4 class="crs-name">${crs[key].Name}</h4>
+          <ul class="crs-graphs"></ul>
+        </div>
+        <div class="crs-opt">
+          <div class="crs-btns"></div>
+          <div class="for-price">
+            <span class="crs-tip">${crs[key].Period}</span>
+            <span class="crs-price">${crs[key].Price}<sup>€</sup></span>
+          </div>
+        </div>
+      </div>`;
     
+      // Create course paragraphs
     let coursesList = document.querySelectorAll('.crs-graphs');
-    let listArr = [];
+    let coursesTxt = crs[key].Txt[0]; // Get obj paragraphs from JSON
+    let listArr = []; // Array
     for (let i = 0; i < coursesList.length; i++) {
       listArr.push(i);
       if (listArr[i] == keyArr[key]) {
-        let coursesTxt = crs[key].Txt[0];
         for (let paragraph in coursesTxt) {
-          // console.log(listArr[i]);
           coursesList[i].innerHTML += `<li>${coursesTxt[paragraph]}</li>`;
         }
       }
     }
+      // Create social icons
     let coursesBtns = document.querySelectorAll('.crs-btns');
-    let btnArr = [];
+    let coursesSocBtns = crs[key].Btns[0]; // Get obj btns from JSON
+    let btnArr = []; // Array
     for (let i = 0; i < coursesBtns.length; i++) {
       btnArr.push(i);
-
       if (btnArr[i] == keyArr[key]) {
-
-        let coursesSocBtns = crs[key].Btns[0];
-          // Create obj. for generate special icons
-        let icons = {
-          map : coursesSocBtns.map,
-          tel : coursesSocBtns.tel,
-          mail : coursesSocBtns.mail,
-          facebook : coursesSocBtns.facebook,
-          instagram : coursesSocBtns.instagram,
-          telegram : coursesSocBtns.telegram,
-          vk : coursesSocBtns.vk
-        }
-        // let crsMapJ = crs[key].crsBtns[0].crsMap;
-        console.log(icons.mail);
-        
         for (let contactBtn in coursesSocBtns) {
-          // console.log(btnArr[i]);
+          // console.log(contactBtn);
           coursesBtns[i].innerHTML += 
           `<a href="${coursesSocBtns[contactBtn]}">
             <svg role="img" class="crs-svg">
-              <use xlink:href="./img/icons/icons.svg#geo"></use>
+              <use xlink:href="./img/icons/icons.svg#${contactBtn}"></use>
             </svg>
-          </a>`;        
+          </a>`;
         }
       }
-    }
+    }  
   }
+  // console.log(keyArr);
+  
 }
+
+let x = 0;
+let getNum = () => x += 10;
+let btnJsonParse = document.querySelector('#btJsonParse');
+btnJsonParse.addEventListener('click', function () {
+  z =  getNum();
+
+  console.log( z );
+}); 
+
 
